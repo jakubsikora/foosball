5b6a1e6478754092d1a85649d4dcf120682fa18d
Jakub Sikora <jakub.sikora@pure360.com>
2015-01-05 16:28:49 +0000

    testing buildpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -168,4 +168,7 @@ module.exports = function(grunt) {
 
 	// Test task.
 	grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
+
+	// Heroku build
+	grunt.registerTask('heroku:production', ['loadConfig', 'ngAnnotate', 'uglify', 'cssmin']);
 };
