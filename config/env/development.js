446b32f38b61957c96556b6721b9218d3b8df68f
Steve Thomson <organicpanda@gmail.com>
2015-01-20 22:18:48 +0000

    Player list on the create page

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://foosball:Foosball1!@ds027751.mongolab.com:27751/foosball',
+	db: 'mongodb://localhost:27017/foosball',
 	app: {
 		title: 'Foosball - Development Environment'
 	},
--- public/modules/scores/controllers/scores.client.controller.js
+++ public/modules/scores/controllers/scores.client.controller.js
@@ -1,11 +1,17 @@
 'use strict';
 
 // Scores controller
-angular.module('scores').controller('ScoresController', ['$scope', '$stateParams', '$location', 'Authentication', 'Scores',
-	function($scope, $stateParams, $location, Authentication, Scores ) {
+angular.module('scores').controller('ScoresController', ['$scope', '$stateParams', '$location', 'Authentication', 'Scores', 'scoreUtils',
+	function($scope, $stateParams, $location, Authentication, Scores, scoreUtils) {
 		$scope.authentication = Authentication;
 
-    $scope.data = {};
-    $scope.data.scores = Scores.query();
+    $scope.data = {
+      scores: Scores.query(),
+      players: []
+    };
+
+    $scope.data.scores.$promise.then(function(scores) {
+      $scope.data.players = scoreUtils.getPlayers(scores);
+    });
   }
 ]);
