dfc4eec9098b47ab5fde78dc537d1666962601d1
Steve Thomson <organicpanda@gmail.com>
2015-01-25 21:32:34 +0000

    Clean up

--- app/models/score.server.model.js
+++ app/models/score.server.model.js
@@ -4,33 +4,33 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
 
 var ScoreSchema = new Schema({
-	teamA: {
-		type: Array,
-		required: 'Team A is required'
-	},
-	teamB: {
-		type: Array,
-		required: 'Team B is required'
-	},
-	teamAScore: {
-		type: Number,
-		default: 0,
-		required: 'Team A Score is required'
-	},
-	teamBScore: {
-		type: Number,
-		default: 0,
-		required: 'Team B Score is required'
-	},
-	saved: {
-		type: Date,
-		default: Date.now
-	}
-	// TODO: user through basic auth
-	// user: {
-	// 	type: Schema.ObjectId,
-	// 	ref: 'User'
-	// }
+  teamA: {
+    type: Array,
+    required: 'Team A is required'
+  },
+  teamB: {
+    type: Array,
+    required: 'Team B is required'
+  },
+  teamAScore: {
+    type: Number,
+    default: 0,
+    required: 'Team A Score is required'
+  },
+  teamBScore: {
+    type: Number,
+    default: 0,
+    required: 'Team B Score is required'
+  },
+  saved: {
+    type: Date,
+    default: Date.now
+  }
+  // TODO: user through basic auth
+  // user: {
+  //  type: Schema.ObjectId,
+  //  ref: 'User'
+  // }
 });
 
 mongoose.model('Score', ScoreSchema);
